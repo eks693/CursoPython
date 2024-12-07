0eb549d4f86d9a93fb51af14f0565c105cefdcb5
eks693 <erickorlando@gmail.com>
2024-12-07 13:38:14 -0300

    Delete meu_pacote directory

--- meu_pacote/modulo_saudacao.py
+++
@@ -1,8 +0,0 @@
-def saudacao(nome):
-    return f"Olá, seja bem vindo {nome}!"
-
-def escolha(nome):
-    return f"Olá, {nome}! Escolha qual operação deseja?"
-
-def resposta(nome):
-    return f"A resposta da sua operação é: "
